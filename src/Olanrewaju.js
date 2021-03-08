9f470e57d8a0ed2da0095cc246c70cef5e28edf0
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-03-09 00:09:32 +0100

    chaka proposal

---
+++ src/Olanrewaju.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Olanrewaju = () => {
+  return (
+    <>
+      <div className="heading">
+        <span>hi chaka. heard you in need of a </span>
+        <span>FULLSTACK Developer</span>
+        <p>
+          well look no further i got you covered. my name is OLAYIWOLA
+          Olanrewaju and i am here to fill that position armed with React
+          and Nodejs,other technology in my armory include Redux,Tailwind,
+          nodejs,Express,mongoDb,mySql and my tenacity at problem-solving,above all i
+          pride myself in ability to pick up new technology quickly. 
+          <a href="http://olanrewaju.herokuapp.com/">portfolio</a>
+          <a href="https://www.linkedin.com/in/olanrewaju-olayiwola/">linkedin</a>
+          <a href="https://github.com/larrycoal">github</a>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Olanrewaju;
